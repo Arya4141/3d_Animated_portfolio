5656df6c4d4855f8d29cecb0073fec4e5fca8e66
Arya4141 <aryaagrawal414@gmail.com>
2024-01-20 23:40:43 +0530

    First Comit

---
+++ src/assets/icons/index.js
@@ -0,0 +1,61 @@
+import css from './css.svg'
+import express from './express.svg'
+import git from './git.svg'
+import github from './github.svg'
+import html from './html.svg'
+import javascript from './javascript.svg'
+import mongodb from './mongodb.svg'
+import motion from './motion.png'
+import mui from './mui.svg'
+import nextjs from './nextjs.png'
+import nodejs from './nodejs.svg'
+import react from './react.svg'
+import redux from './redux.svg'
+import sass from './sass.svg'
+import tailwindcss from './tailwindcss.svg'
+import typescript from './typescript.svg'
+import linkedin from './linkedin.svg'
+import twitter from './twitter.svg'
+import car from './car.png'
+import estate from './estate.png'
+import pricewise from './pricewise.png'
+import snapgram from './snapgram.svg'
+import summiz from './summiz.png'
+import threads from './threads.png'
+import arrow from './arrow.svg'
+import contact from './contact.png'
+import soundon from './soundon.png'
+import soundoff from './soundoff.png'
+import rating from './rating.png'
+
+export {
+    css,
+    express,
+    git,
+    github,
+    html,
+    javascript,
+    mongodb,
+    motion,
+    mui,
+    nextjs,
+    nodejs,
+    react,
+    redux,
+    sass,
+    tailwindcss,
+    typescript,
+    linkedin,
+    twitter,
+    car,
+    estate,
+    pricewise,
+    snapgram,
+    summiz,
+    threads,
+    arrow,
+    contact,
+    soundon,
+    rating,
+    soundoff
+}
---
+++ src/constants/index.js
@@ -0,0 +1,214 @@
+import { meta, shopify, starbucks, tesla } from "../assets/images";
+import {
+    car,
+    contact,
+    css,
+    estate,
+    rating,
+    express,
+    git,
+    github,
+    html,
+    javascript,
+    linkedin,
+    mongodb,
+    motion,
+    mui,
+    nextjs,
+    nodejs,
+    pricewise,
+    react,
+    redux,
+    sass,
+    snapgram,
+    summiz,
+    tailwindcss,
+    threads,
+    typescript
+} from "../assets/icons";
+
+export const skills = [
+    {
+        imageUrl: css,
+        name: "CSS",
+        type: "Frontend",
+    },
+    {
+        imageUrl: git,
+        name: "Git",
+        type: "Version Control",
+    },
+    {
+        imageUrl: github,
+        name: "GitHub",
+        type: "Version Control",
+    },
+    {
+        imageUrl: html,
+        name: "HTML",
+        type: "Frontend",
+    },
+    {
+        imageUrl: javascript,
+        name: "JavaScript",
+        type: "Frontend",
+    },
+    {
+        imageUrl: mongodb,
+        name: "MongoDB",
+        type: "Database",
+    },
+    {
+        imageUrl: motion,
+        name: "Motion",
+        type: "Animation",
+    },
+    {
+        imageUrl: mui,
+        name: "Material-UI",
+        type: "Frontend",
+    },
+    {
+        imageUrl: nextjs,
+        name: "Next.js",
+        type: "Frontend",
+    },
+    {
+        imageUrl: nodejs,
+        name: "Node.js",
+        type: "Backend",
+    },
+    {
+        imageUrl: react,
+        name: "React",
+        type: "Frontend",
+    },
+    {
+        imageUrl: redux,
+        name: "Redux",
+        type: "State Management",
+    },
+    {
+        imageUrl: sass,
+        name: "Sass",
+        type: "Frontend",
+    },
+    {
+        imageUrl: tailwindcss,
+        name: "Tailwind CSS",
+        type: "Frontend",
+    },
+    {
+        imageUrl: typescript,
+        name: "TypeScript",
+        type: "Frontend",
+    }
+];
+
+export const experiences = [
+    {
+        title: "Infosys Springboard ",
+        company_name: "-Infosys",
+        icon: starbucks,
+        iconBg: "#accbe1",
+        date: "Infosys",
+        points: [],
+    },
+    {
+        title: "Data Structure",
+        company_name: "-Coding Ninjas",
+        icon: tesla,
+        iconBg: "#fbc3bc",
+        date: "Coding Ninjas",
+        points: [],
+    },
+    {
+        title: "MERN Stack Developer",
+        company_name: "-Codehelp",
+        icon: shopify,
+        iconBg: "#b7e4c7",
+        date: "Codehelp",
+        points: [],
+    },
+    {
+        title: "10FastFingers Certified in Fast typing",
+        company_name: "-10FastFingers",
+        icon: meta,
+        iconBg: "#a2d2ff",
+        date: "10FastFingers",
+        points: [],
+    },
+    {
+        title: "Basics of Java ",
+        company_name: "-Coding Ninjas",
+        icon: tesla,
+        iconBg: "#fbc3bc",
+        date: "Coding Ninjas",
+        points: [],
+    },
+    
+];
+
+export const socialLinks = [
+    {
+        name: 'Contact',
+        iconUrl: contact,
+        link: 'https://www.instagram.com/accounts/onetap/?next=%2F',
+    },
+    {
+        name: 'GitHub',
+        iconUrl: github,
+        link: 'https://github.com/Arya4141',
+    },
+    {
+        name: 'LinkedIn',
+        iconUrl: linkedin,
+        link: 'https://www.linkedin.com/in/arya-agrawal-216621220/',
+    }
+];
+
+export const projects = [
+    {
+        iconUrl: pricewise,
+        theme: 'btn-back-red',
+        name: 'Food Delivery Site',
+        description: 'Designed a web app for effortless food ordering, ensuring swift delivery of users favorite meals by prioritizing efficiency and convenience.',
+        link: 'https://github.com/Arya4141/Food-Delivery-Website.git',
+    },
+    {
+        iconUrl: threads,
+        theme: 'btn-back-green',
+        name: 'Aura Wheel Ventures',
+        description: 'Effortless bike rental system: users easily book or list bikes, streamlining the sharing economy with convenience, reliability, and accessibility.',
+        link: 'https://github.com/Arya4141/Bike-Rental-System.git',
+    },
+    {
+        iconUrl: car,
+        theme: 'btn-back-pink',
+        name: 'Full Stack Instagram Clone',
+        description: 'Built a complete clone of Instagram, allowing users to share photos and connect with friends in a familiar social media environment.',
+        link: 'https://github.com/Arya4141/INSTAGRAM_LIVE.git',
+    },
+    
+    {
+        iconUrl: estate,
+        theme: 'btn-back-black',
+        name: 'Random GIF Generator',
+        description: 'Create and enjoy custom GIFs on-demand by entering topics, or embrace spontaneity with random GIF generation.',
+        link: 'https://github.com/Arya4141/Random_GIF_Generator',
+    },
+    {
+        iconUrl: summiz,
+        theme: 'btn-back-yellow',
+        name: 'Finovo',
+        description: 'A cutting-edge full-stack application revolutionizing stock management with live market insights, advanced analytics, and real-time news, enhancing financial decision-making.',
+        link: 'https://github.com/Arya4141/FINOVO.git',
+    },
+    {
+        iconUrl: rating,
+        theme: 'btn-back-green',
+        name: 'IMDB rating site',
+        description: "IMDb rating site created that provides the rating of latest movies and web-series crafted with user-friendly design. Data is fetched through real-time API's and provides ",
+        link: 'https://github.com/Arya4141/Bike-Rental-System.git',
+    },
+];
